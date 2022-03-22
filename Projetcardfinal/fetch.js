85c5b2dce8d5bb7e3488a71193762b3d32a6a446
Desire Maya <desiremrv@gmail.com>
2022-03-22 23:48:50 +0100

    cambios en js y css

--- Projetcardfinal/fetch.js
+++ Projetcardfinal/fetch.js
@@ -1,54 +1,30 @@
-const textoPrueba ="hola";
+const textoPrueba = "hola";
 const tabla = document.querySelector("#info");
 var alimentosConsumo = [];
 
-function cargarAlimentos(){
-    
-    fetch('talimentos.json')
-
-        .then(respuesta => respuesta.json()) //Indicamos el formato en que se desea obtener la información
-        .then(alimentos => {
-            alimentos.forEach((alimento, contador) => {  
-               
-                
-                const row = document.createElement('div');
-                row.classList.add("card-container");
-                row.innerHTML += `                    
+function cargarAlimentos() {
+  fetch("talimentos.json")
+    .then((respuesta) => respuesta.json()) //Indicamos el formato en que se desea obtener la información
+    .then((alimentos) => {
+      alimentos.forEach((alimento, contador) => {
+        const row = document.createElement("div");
+        row.classList.add("card-container");
+        row.innerHTML += `                    
                 
                     <p>${alimento.nombre}</p>
-                    <p>${alimento.medida}</p>
-                    <p>${alimento.equivalencia} ${alimento.unidad || 'gr'}</p>
-                   
                     <img src=${alimento.src} id="imagen">
                     <br>
                     <input type="number" id="gramos-alimento-${contador}" placeholder="0" onchange="alimentosConsumo[${contador}] = this.value" class="hola">                   
                     
                 `;
-                tabla.appendChild(row);
-
-             
-                               
-            });
-        }) // Aquí mostramos dicha información
-        .catch(error => console.log('Hubo un error : ' + error.message))
-
-        
+        tabla.appendChild(row);
+      });
+    }) // Aquí mostramos dicha información
+    .catch((error) => console.log("Hubo un error : " + error.message));
 }
-
 
 cargarAlimentos();
 
-
-
-function saveAlimentosConsumo(){
-    localStorage.setItem('alimentosConsumo',JSON.stringify(alimentosConsumo));
+function saveAlimentosConsumo() {
+  localStorage.setItem("alimentosConsumo", JSON.stringify(alimentosConsumo));
 }
-
-
-
-
-
-
-
-
-
